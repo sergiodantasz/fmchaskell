452564e979c100840825b848a3b8e18befcb8026
Sérgio Dantas <sergiodnts828@gmail.com>
2025-09-30 18:08:42 -0300

    implement zip

--- Solutions/FMCList.hs
+++ Solutions/FMCList.hs
@@ -277,6 +277,11 @@ isSufixOf :: Eq a => [a] -> [a] -> Bool
 isSufixOf xs ys = reverse xs `isPrefixOf` reverse ys
 
 -- zip
+zip :: [a] -> [b] -> [(a, b)]
+zip _ [] = []
+zip [] _ = []
+zip (x : xs) (y : ys) = (x, y) : zip xs ys
+
 -- zipWith
 
 -- intercalate
