8b1f2a2f3fe876c1ff0125db76fb313a5f649f46
Sérgio Dantas <sergiodnts828@gmail.com>
2025-09-30 09:06:28 -0300

    implement dropWhile

--- Solutions/FMCList.hs
+++ Solutions/FMCList.hs
@@ -159,6 +159,11 @@ takeWhile p (x : xs)
   | otherwise = []
 
 -- dropWhile
+dropWhile :: (a -> Bool) -> [a] -> [a]
+dropWhile _ [] = []
+dropWhile p (x : xs)
+  | p x       = dropWhile p xs
+  | otherwise = x : xs
 
 -- tails
 -- init
