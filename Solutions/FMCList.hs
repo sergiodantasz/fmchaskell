851402781194d0442a5accccf74929bee7cb0406
Sérgio Dantas <sergiodnts828@gmail.com>
2025-10-01 18:54:27 -0300

    refactor FMCList.hs

--- Solutions/FMCList.hs
+++ Solutions/FMCList.hs
@@ -31,149 +31,113 @@ import Prelude
   )
 import Prelude qualified as P
 
-{- import qualified ... as ... ?
-
-To use a function from a qualified import
-you need to prefix its name with its alias and a dot:
-P.head   C.toUpper   etc.
-
-I import these for you to test the original functions on ghci:
-
-ghci> :t C.toUpper
-C.toUpper :: Char -> Char
-
-You MUST NOT use ANY of these in your code
-
--}
-
-{- Our lists vs Haskell lists
-
-Our definition:
-
-data List a where
-  Nil  :: List a
-  Cons :: a -> List a -> List a
-
-Here we use Haskell's built-in lists and associated syntactic sugar.
-It is as if it was defined like this:
-
-    data [a] = [] | (x : xs)
-
-or like this:
-
-    data [a] where
-      []  :: [a]
-      (:) :: a -> [a] -> [a]
-
-write [a]       for our List a
-write []        for our List
-write []        for our Nil
-write (x : xs)  for our Cons x xs
-write [u,v]     for our u `Cons` (v `Cons` Nil)
-
--}
-
+-- head
 head :: [a] -> a
-head []      = undefined
+head [] = error "head called with an empty list"
 head (x : _) = x
 
+-- tail
 tail :: [a] -> [a]
-tail []       = undefined
+tail [] = error "tail called with an empty list"
 tail (_ : xs) = xs
 
+-- null
 null :: [a] -> Bool
 null [] = True
-null _  = False
+null _ = False
 
+-- length
 length :: (Integral i) => [a] -> i
-length []       = 0
+length [] = 0
 length (_ : xs) = 1 + length xs
 
+-- sum
 sum :: (Num a) => [a] -> a
-sum []       = 0
+sum [] = 0
 sum (x : xs) = x + sum xs
 
+-- product
 product :: (Num a) => [a] -> a
-product []       = 1
+product [] = 1
 product (x : xs) = x * product xs
 
+-- (**)
 (++) :: [a] -> [a] -> [a]
-[]       ++ ys = ys
+[] ++ ys = ys
 (x : xs) ++ ys = x : (xs ++ ys)
 
--- right-associative for performance!
--- (what?!)
 infixr 5 ++
 
+-- reverse
 reverse :: [a] -> [a]
-reverse []       = []
+reverse [] = []
 reverse (x : xs) = reverse xs ++ [x]
 
--- (snoc is cons written backwards)
+-- snoc
+-- it is cons written backwards
 snoc :: a -> [a] -> [a]
-snoc x []       = [x]
+snoc x [] = [x]
 snoc x (y : ys) = y : snoc x ys
 
 (<:) :: [a] -> a -> [a]
 (<:) = flip snoc
 
--- different implementation of (++)
+-- (+++)
+-- it is a different implementation of (++)
 (+++) :: [a] -> [a] -> [a]
-xs +++ []       = xs
-xs +++ [y]      = xs <: y
+xs +++ [] = xs
+xs +++ [y] = xs <: y
 xs +++ (y : ys) = (xs +++ [y]) +++ ys
 
--- left-associative for performance!
--- (hmm?!)
 infixl 5 +++
 
 -- minimum
-minimum :: Ord a => [a] -> a
-minimum []       = undefined
-minimum [x]      = x
+minimum :: (Ord a) => [a] -> a
+minimum [] = error "minimum called with an empty list"
+minimum [x] = x
 minimum (x : xs) = min x (minimum xs)
 
 -- maximum
-maximum :: Ord a => [a] -> a
-maximum []       = undefined
-maximum [x]      = x
+maximum :: (Ord a) => [a] -> a
+maximum [] = error "maximum called with an empty list"
+maximum [x] = x
 maximum (x : xs) = max x (maximum xs)
 
 -- take
 take :: (Integral i) => i -> [a] -> [a]
-take 0 _        = []
-take _ []       = []
+take x _ | x <= 0 = []
+take _ [] = []
 take x (y : ys) = y : take (x - 1) ys
 
 -- drop
 drop :: (Integral i) => i -> [a] -> [a]
-drop 0 xs       = xs
-drop _ []       = []
+drop x xs | x <= 0 = xs
+drop _ [] = []
 drop x (_ : ys) = drop (x - 1) ys
 
 -- takeWhile
 takeWhile :: (a -> Bool) -> [a] -> [a]
 takeWhile _ [] = []
 takeWhile p (x : xs)
-  | p x       = x : takeWhile p xs
+  | p x = x : takeWhile p xs
   | otherwise = []
 
 -- dropWhile
 dropWhile :: (a -> Bool) -> [a] -> [a]
 dropWhile _ [] = []
 dropWhile p (x : xs)
-  | p x       = dropWhile p xs
+  | p x = dropWhile p xs
   | otherwise = x : xs
 
 -- tails
 tails :: [a] -> [[a]]
-tails []       = [[]]
+tails [] = [[]]
 tails (x : xs) = (x : xs) : tails xs
 
 -- init
 init :: [a] -> [a]
-init []       = undefined
-init [_]      = []
+init [] = error "init called with an empty list"
+init [_] = []
 init (x : xs) = x : init xs
 
 -- inits
@@ -183,7 +147,7 @@ inits xs = xs : inits (init xs)
 
 -- subsequences
 subsequences :: [a] -> [[a]]
-subsequences []       = [[]]
+subsequences [] = [[]]
 subsequences (x : xs) =
   let tailSubs = subsequences xs
    in tailSubs ++ map (x :) tailSubs
@@ -192,64 +156,66 @@ subsequences (x : xs) =
 any :: (a -> Bool) -> [a] -> Bool
 any _ [] = False
 any p (x : xs)
-  | p x       = True
+  | p x = True
   | otherwise = any p xs
 
 -- all
 all :: (a -> Bool) -> [a] -> Bool
 all _ [] = True
 all p (x : xs)
-  | p x       = all p xs
+  | p x = all p xs
   | otherwise = False
 
 -- and
 and :: [Bool] -> Bool
-and []       = True
+and [] = True
 and (b : bs) = b && and bs
 
 -- or
 or :: [Bool] -> Bool
-or []       = False
+or [] = False
 or (b : bs) = b || or bs
 
 -- concat
 concat :: [[a]] -> [a]
-concat []       = []
+concat [] = []
 concat (l : ls) = l ++ concat ls
 
--- elem using the funciton 'any' above
-elem :: Eq a => a -> [a] -> Bool
+-- elem
+-- (using the function 'any' above)
+elem :: (Eq a) => a -> [a] -> Bool
 elem x = any (== x)
 
--- elem': same as elem but elementary definition
--- (without using other functions except (==))
-elem' :: Eq a => a -> [a] -> Bool
+-- elem'
+-- same as elem but elementary definition (without using other functions except (==))
+elem' :: (Eq a) => a -> [a] -> Bool
 elem' _ [] = False
 elem' x (y : ys)
-  | x == y    = True
+  | x == y = True
   | otherwise = elem' x ys
 
 -- (!!)
-(!!) :: [a] -> Int -> a
-[]       !! _ = undefined
-(x : _)  !! 0 = x
+(!!) :: (Integral i) => [a] -> i -> a
+_ !! i | i < 0 = error "(!!) called with a negative index"
+[] !! _ = error "(!!) called with an empty list"
+(x : _) !! 0 = x
 (_ : xs) !! i = xs !! (i - 1)
 
 -- filter
 filter :: (a -> Bool) -> [a] -> [a]
 filter _ [] = []
 filter p (x : xs)
-  | p x       = x : filter p xs
+  | p x = x : filter p xs
   | otherwise = filter p xs
 
 -- map
 map :: (a -> b) -> [a] -> [b]
-map _ []       = []
+map _ [] = []
 map f (x : xs) = f x : map f xs
 
 -- cycle
 cycle :: [a] -> [a]
-cycle [] = undefined
+cycle [] = error "cycle called with an empty list"
 cycle xs = xs ++ cycle xs
 
 -- repeat
@@ -257,23 +223,23 @@ repeat :: a -> [a]
 repeat x = cycle [x]
 
 -- replicate
-replicate :: Int -> a -> [a]
-replicate n x = take n (repeat x)
+replicate :: (Integral i) => i -> a -> [a]
+replicate i x = take i (repeat x)
 
 -- isPrefixOf
-isPrefixOf :: Eq a => [a] -> [a] -> Bool
+isPrefixOf :: (Eq a) => [a] -> [a] -> Bool
 isPrefixOf [] _ = True
 isPrefixOf _ [] = False
 isPrefixOf (x : xs) (y : ys) = x == y && isPrefixOf xs ys
 
 -- isInfixOf
-isInfixOf :: Eq a => [a] -> [a] -> Bool
+isInfixOf :: (Eq a) => [a] -> [a] -> Bool
 isInfixOf [] _ = True
 isInfixOf _ [] = False
-isInfixOf xs ys@(y : ys') = isPrefixOf xs ys || isInfixOf xs ys'
+isInfixOf xs (y : ys) = isPrefixOf xs (y : ys) || isInfixOf xs ys
 
 -- isSuffixOf
-isSuffixOf :: Eq a => [a] -> [a] -> Bool
+isSuffixOf :: (Eq a) => [a] -> [a] -> Bool
 isSuffixOf xs ys = reverse xs `isPrefixOf` reverse ys
 
 -- zip
@@ -295,29 +261,26 @@ intercalate _ [ys] = ys
 intercalate xs (ys : yss) = ys ++ xs ++ intercalate xs yss
 
 -- nub
-nub :: Eq a => [a] -> [a]
+nub :: (Eq a) => [a] -> [a]
 nub [] = []
 nub (x : xs) = x : nub (filter (x /=) xs)
 
 -- splitAt
--- what is the problem with the following?:
--- splitAt n xs  =  (take n xs, drop n xs)
--- the problem is performance! the function iterates through the list two times.
-splitAt :: Int -> [a] -> ([a], [a])
-splitAt 0 xs = ([], xs)
+splitAt :: (Integral i) => i -> [a] -> ([a], [a])
+splitAt i xs | i <= 0 = ([], xs)
 splitAt _ [] = ([], [])
-splitAt n (x : xs) =
-  let (leftPart, rightPart) = splitAt (n - 1) xs
+splitAt i (x : xs) =
+  let (leftPart, rightPart) = splitAt (i - 1) xs
    in (x : leftPart, rightPart)
 
 -- break
 break :: (a -> Bool) -> [a] -> ([a], [a])
 break _ [] = ([], [])
 break p (x : xs)
-  | p x       = ([], x : xs)
+  | p x = ([], x : xs)
   | otherwise =
-    let (ys, zs) = break p xs
-     in (x : ys, zs)
+      let (ys, zs) = break p xs
+       in (x : ys, zs)
 
 -- lines
 lines :: String -> [String]
@@ -333,11 +296,13 @@ words :: String -> [String]
 words s =
   case dropWhile C.isSpace s of
     "" -> []
-    s' -> let (word, rem) = break C.isSpace s'
-           in word : words rem
+    s' ->
+      let (word, rem) = break C.isSpace s'
+       in word : words rem
 
 -- unlines
 unlines :: [String] -> String
+unlines [] = ""
 unlines s = intercalate "\n" s ++ "\n"
 
 -- unwords
@@ -358,20 +323,7 @@ normalize :: String -> String
 normalize s = filter C.isAlphaNum (map C.toLower s)
 
 -- palindrome
--- checks if the letters of a phrase form a palindrome (see below for examples)
 palindrome :: String -> Bool
 palindrome s =
   let normS = normalize s
    in normS == reverse normS
-
-{-
-
-Examples of palindromes:
-
-"Madam, I'm Adam"
-"Step on no pets."
-"Mr. Owl ate my metal worm."
-"Was it a car or a cat I saw?"
-"Doc, note I dissent.  A fast never prevents a fatness.  I diet on cod."
-
--}
